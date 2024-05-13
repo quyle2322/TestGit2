c213629209ab6e02fbc1b8ca28b001b64e5d0a04
go-up-now <nguyenhuunghia@gmail.com>
2024-05-13 15:07:20 +0700

    update service

--- src/main/java/com/example/lab2/service/TestService.java
+++ src/main/java/com/example/lab2/service/TestService.java
@@ -2,13 +2,18 @@ package com.example.lab2.service;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class TestService {
     @GetMapping("/test")
-    public ResponseEntity<?> doGetData(){
-
-        return ResponseEntity.ok("Hello World");
+    public ResponseEntity<?> doGetData(@RequestParam("name") String name){
+        Map<String, String> user = new HashMap<>();
+        user.put("name", name);
+        return ResponseEntity.ok(user);
     }
 }
